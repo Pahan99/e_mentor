63988f480b4492a3cd1269016a738afa4de02e3d
Pahan99 <66349048+Pahan99@users.noreply.github.com>
2021-11-22 18:04:57 +0530

    Modified home page

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -5,12 +5,31 @@ namespace app\controllers;
 use app\core\Application;
 use app\core\Controller;
 use app\core\Request;
+use app\models\Resource;
+use app\models\User;
 
 class SiteController extends Controller
 {
     public function welcome()
     {
-        return $this->render('welcome');
+        $resourceModel = new Resource();
+        $userModel = new User();
+
+        $resourceList = $resourceModel->getAll();
+        $userList = $userModel->getAll(
+            []
+        );
+        $userList = array_filter($userList, function ($user) {
+            return $user["role_id"] != '1' && $user["role_id"] != '5';
+        });
+//        echo '<pre>';
+//        var_dump($userList);
+//        echo '</pre>';
+//        exit();
+        return $this->render('welcome', [
+            'resources' => $resourceList,
+            'counsellors' => $userList
+        ]);
     }
 
     public function profile()

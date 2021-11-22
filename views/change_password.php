51f0242f57571efc8d2bca64e991b6e33a1b96ba
AwesomeNipun <54329644+AwesomeNipun@users.noreply.github.com>
2021-11-23 02:07:12 +0530

    validation 1.0

--- views/change_password.php
+++ views/change_password.php
@@ -1,12 +1,13 @@
+<?php echo $error ?? '' ?>
 <h1>Change Password</h1>
 <div class="container" >
-    <form>
+    <form method="post" action="">
         <label>Current Password:</label>
         <input type="password" name="curr_pass" value = ""> <br><br>
         <label>New Password:</label>
         <input type="password" name="new_pass" value=""><br><br>
         <label>Confirm Password:</label>
         <input type="password" name="confirm_pass" value=""><br>
+        <button type="submit" class='done_btn'>Done</button>
     </form>
-    <button onclick="location.href = 'edit_user_profile.php'" class='done_btn'>Done</button>
 </div>
--- views/edit_user_profile.php
+++ views/edit_user_profile.php
@@ -1,17 +1,20 @@
-<?php /**@var $user array */
 
+<?php /**@var $user array */
 ?>
 <h1>Edit User Profile</h1>
 <div class="container" >
     <form action="" method="post">
+        <div class="col-lg-3">
         <label>Full Name:</label><br>
-        <input type="text" name="name" value = <?php echo $user["name"] ?>> <br><br>
+        <input class="form-control" type="text" name="name" value = <?php echo $user['name'] ?>> <br><br>
         <label>Email:</label><br>
-        <input type="text" name="email" value=<?php echo $user["email"] ?> readonly id='email'><br><br>
+        <input class="form-control" type="text" name="email" value=<?php echo $user['email'] ?> readonly id='email'><br><br>
         <label>Contact Number:</label><br>
-        <input type="text" name="contact_no" value=<?php echo $user["contact_no"] ?>><br><br>
+        <input class="form-control" type="text" name="contact_no" value=<?php echo $user['contact_no'] ?>><br><br>
+            <a href="/change_password" class="btn btn-success">Change Password</a><br><br>
         <button name="done", class="btn btn-primary">Done</button>
-        <a class="btn btn-secondary" href="/dashboard">Cancel</a>
+        <a class="btn btn-danger" href="/dashboard">Cancel</a>
+        </div>
     </form>
-    <button onclick="location.href = 'change_password.php'" class='changebtn'>Change Password</button>
 </div>
+
